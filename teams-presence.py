3caeed0f75c6d697deee850565ba98dc2d0be6a6
Chris Hemingway <chris.hemingway@gmail.com>
2022-02-01 20:16:11 +0000

    Remove pyunicorn and blinking/flashing

--- teams-presence.py
+++ teams-presence.py
@@ -45,7 +45,6 @@ try:
 	import configparser
 	from urllib.error import HTTPError
 	import json
-	import unicornhat as unicorn
 	import threading
 	import sys
 	import urllib.parse
@@ -215,151 +214,28 @@ def checkUpdate():
 		printerror(e)
 
 # ############################
-#        UNICORN SETUP
+#        Light Beacon Output
 # ############################
-def setColor(r, g, b, brightness, speed) :
-	global crntColors, globalBlue, globalGreen, globalRed
-	globalRed = r
-	globalGreen = g
-	globalBlue = b
-
-	if brightness == '' :
-		unicorn.brightness(brightness_led)
-
-	for y in range(height):
-		for x in range(width):
-			unicorn.set_pixel(x, y, r, g, b)
-			unicorn.show()
-
 def pulse():
-	for b in range(0, 7):
-		blockPrint()
-		unicorn.brightness(b/10)
-		enablePrint()
-		for y in range(height):
-			for x in range(width):
-				unicorn.set_pixel(x, y, 102, 255, 255)
-				unicorn.show()
-		sleep(0.05)
-	sleep(1)
-	for b in range(6, 0, -1):
-		blockPrint()
-		unicorn.brightness(b/10)
-		enablePrint()
-		for y in range(height):
-			for x in range(width):
-				unicorn.set_pixel(x, y, 102, 255, 255)
-				unicorn.show()
-		sleep(0.05)
+	pass
 
 def switchBlue() :
-	red = 0
-	green = 0
-	blue = 250
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, '', ''))
-	blinkThread.do_run = True
-	blinkThread.start()
+	pass
 
 def switchRed() :
-	red = 250
-	green = 0
-	blue = 0
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, '', ''))
-	blinkThread.do_run = True
-	blinkThread.start()
+	pass
 
 def switchGreen() :
-	red = 0
-	green = 250
-	blue = 0
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, '', ''))
-	blinkThread.do_run = True
-	blinkThread.start()
+	pass
 
 def switchPink() :
-	red = 255
-	green = 108
-	blue = 180
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, '', ''))
-	blinkThread.do_run = True
-	blinkThread.start()
+	pass
 
 def switchYellow() :
-	red = 255
-	green = 255
-	blue = 0
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, '', ''))
-	blinkThread.do_run = True
-	blinkThread.start()
+	pass
 
 def switchOff() :
-	global blinkThread, globalBlue, globalGreen, globalRed
-	globalRed = 0
-	globalGreen = 0
-	globalBlue = 0
-	if blinkThread != None :
-		blinkThread.do_run = False
-	unicorn.clear()
-	unicorn.off()
-
-class LightPoint:
-
-	def __init__(self):
-		self.direction = randint(1, 4)
-		if self.direction == 1:
-			self.x = randint(0, width - 1)
-			self.y = 0
-		elif self.direction == 2:
-			self.x = 0
-			self.y = randint(0, height - 1)
-		elif self.direction == 3:
-			self.x = randint(0, width - 1)
-			self.y = height - 1
-		else:
-			self.x = width - 1
-			self.y = randint(0, height - 1)
-
-		self.colour = []
-		for i in range(0, 3):
-			self.colour.append(randint(100, 255))
-
-
-def update_positions():
-
-	for point in points:
-		if point.direction == 1:
-			point.y += 1
-			if point.y > height - 1:
-				points.remove(point)
-		elif point.direction == 2:
-			point.x += 1
-			if point.x > width - 1:
-				points.remove(point)
-		elif point.direction == 3:
-			point.y -= 1
-			if point.y < 0:
-				points.remove(point)
-		else:
-			point.x -= 1
-			if point.x < 0:
-				points.remove(point)
-
-
-def plot_points():
-
-	unicorn.clear()
-	for point in points:
-		unicorn.set_pixel(point.x, point.y, point.colour[0], point.colour[1], point.colour[2])
-	unicorn.show()
-
-def blinkRandom(arg):
-	t = threading.currentThread()
-	while getattr(t, "do_run", True):
-		if len(points) < 10 and randint(0, 5) > 1:
-			points.append(LightPoint())
-		plot_points()
-		update_positions()
-		sleep(0.03)
+	pass
 
 ##################################################
 
@@ -431,10 +307,6 @@ def printHeader():
 
 # Check for Weekend
 def check_weekend():
-	# Stop random blinking
-	blinkThread.do_run = False
-	blinkThread.join()
-
 	now = datetime.now()
 
 	# Check for weekend option
@@ -446,12 +318,7 @@ def check_weekend():
 		now = datetime.now()
 		print("It's " + now.strftime("%A") + ", weekend! Grab more beer! \N{beer mug}")
 		print()
-		if args.nopulse:
-			switchOff()
-		else:
-			pulsethread = threading.Thread(target=pulse)
-			pulsethread.start()
-
+		switchOff()
 		countdown(30)
 
 
@@ -460,21 +327,12 @@ def check_workingtimes():
 	if args.afterwork:
 		return
 
-	# Get CPU temp
-	cpu = CPUTemperature()
-
 	while is_time_between(workday_start, workday_end) == False:
 		printHeader()
 		now = datetime.now()
 		print("Work is over for today, grab a beer! \N{beer mug}")
 		print()
-
-		if args.nopulse:
-			switchOff()
-		else:
-			pulsethread = threading.Thread(target=pulse)
-			pulsethread.start()
-
+		switchOff()
 		countdown(30)
 
 
@@ -492,18 +350,6 @@ if __name__ == '__main__':
 	# Check for updates
 	checkUpdate()
 
-	# Setup Unicorn light
-	setColor(50, 50, 50, 1, '')
-	unicorn.set_layout(unicorn.AUTO)
-	unicorn.brightness(0.5)
-
-	# Get the width and height of the hardware
-	width, height = unicorn.get_shape()
-
-	blinkThread = threading.Thread(target=blinkRandom, args=("task",))
-	blinkThread.do_run = True
-	blinkThread.start()
-
 	trycount = 0
 	while Authorize() == False:
 		trycount = trycount +1
@@ -516,9 +362,6 @@ if __name__ == '__main__':
 			continue
 
 	sleep(1)
-	# Stop random blinking
-	blinkThread.do_run = False
-	blinkThread.join()
 
 	trycount = 0
 
@@ -551,10 +394,6 @@ if __name__ == '__main__':
 				printerror("MS Graph URL is invalid!")
 				exit(5)
 			elif err.response.status_code == 401:
-				blinkThread = threading.Thread(target=blinkRandom, args=("task",))
-				blinkThread.do_run = True
-				blinkThread.start()
-
 				trycount = trycount + 1
 				printerror("MS Graph is not authorized. Please reauthorize the app (401). Trial count: " + str(trycount))
 				print()
@@ -577,33 +416,19 @@ if __name__ == '__main__':
 			countdown(5)
 			continue
 
-		# Stop random blinking
-		if blinkThread != None :
-			blinkThread.do_run = False
-			blinkThread.join()
-
-		# Get CPU temp
-		cpu = CPUTemperature()
-
 		# Print to display
-		os.system('clear')
 		print("============================================")
 		print("            MSFT Teams Presence")
 		print("============================================")
 		print()
 		now = datetime.now()
 		print("Last API call:\t\t" + now.strftime("%Y-%m-%d %H:%M:%S"))
-		cpu_r = round(cpu.temperature, 2)
-		print("Current CPU:\t\t" + str(cpu_r) + "°C")
 
 		if args.brightness:
 			printwarning("Option:\t\t\t" + "Set brightness to " + str(brightness))
 
 		if args.refresh:
 			printwarning("Option:\t\t\t" +  "Set refresh to " + str(sleepValue))
-
-		if args.nopulse:
-			printwarning("Option:\t\t\t" + "Pulsing disabled")
 
 		if args.afterwork:
 			printwarning("Option:\t\t\t" + "Set display after work to True")
